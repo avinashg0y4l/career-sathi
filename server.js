e3e4c2d5f487d8c00da9d4eb08e3fd76438b8120
avinashg0y4l <avinashg0y4l@gmail.com>
2025-08-23 01:41:01 +0530

    Ini Career Sathi project

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mysql = require("mysql2");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -11,13 +12,15 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
 // MySQL connection
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect(err => {
